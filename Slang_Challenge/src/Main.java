d4d09f1de535ed87390d156084727348d1214f15
Valerie Parra Cortés <v.parrac@uniandes.edu.co>
2020-08-06 15:08:19 -0500

    Initial template of challenge

---
+++ Slang_Challenge/src/Main.java
@@ -0,0 +1,21 @@
+import java.io.IOException;
+
+/**
+ * Slang Challenge 04/08/2020
+ * @author Valerie Parra Cortés
+ * Main Class of the challenge. The input will be by console.
+ */
+public class Main {
+	
+	public static void main(String[] args) throws IOException {
+	}
+
+	private static String[] calculateNGrams(String string, int n) {
+	
+		return null;
+	}
+
+	private static String mostFrequentNGram(String string, int n) {
+		return null;
+	}
+}
